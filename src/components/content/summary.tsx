71d4364bf37b93b7b0d8822213301cea47c47174
Landon Schropp <schroppl@gmail.com>
2025-11-18 14:48:44 -0800

    Update Tailwind for v4

--- src/components/content/summary.tsx
+++ src/components/content/summary.tsx
@@ -11,7 +11,7 @@ type SummaryProps = {
 export function Summary({ title, url, children, style }: SummaryProps) {
   return (
     <a
-      className={`border-theme-accent text-theme-text hocus:bg-theme-backgroundHighlight hocus:shadow-largeOutline hocus:shadow-theme-backgroundHighlight my-4 block border-l-[3px] pl-[calc(theme('spacing.3')-3px)] transition-all duration-75 ease-in outline-none`}
+      className={`border-theme-accent text-theme-text hocus:bg-theme-backgroundHighlight hocus:shadow-largeOutline hocus:shadow-theme-backgroundHighlight my-4 block border-l-[3px] pl-[calc(--spacing(3)-3px)] transition-all duration-75 ease-in outline-none`}
       href={url}
       style={style}
     >
