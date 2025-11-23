392d76eab986a64e4135322184c683dd9b0159f2
Landon Schropp <schroppl@gmail.com>
2025-11-22 23:08:37 -0800

    Migrate the notes layout to TanStack

---
+++ src/routes/notes.tsx
@@ -0,0 +1,18 @@
+import { MainNavigation } from "@/components/navigation/main-navigation";
+import { createFileRoute, Outlet } from "@tanstack/react-router";
+
+// TODO: Figure out how to share this layout with the other content layouts.
+export const Route = createFileRoute("/notes")({
+  component: NotesLayout,
+});
+
+export default function NotesLayout() {
+  return (
+    <>
+      <MainNavigation />
+      <main className="mx-auto w-[70ch] max-w-full px-2 md:px-4">
+        <Outlet />
+      </main>
+    </>
+  );
+}
