98232e73584bda377b2336bf31563729ee7a7dea
Landon Schropp <schroppl@gmail.com>
2025-12-07 11:42:44 -0800

    Add article subtypes

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,15 @@
 import {
+  APP_MEDIA,
+  ARTICLE_MEDIA,
+  BOOK_MEDIA,
+  COURSE_MEDIA,
+  LIVE_TALK_MEDIA,
+  PODCAST_MEDIA,
+  RECORDED_TALK_MEDIA,
+  VIDEO_MEDIA,
+  VIDEO_PLAYLIST_MEDIA,
+} from "./constants";
+import {
   ArticleSchema,
   CategorySchema,
   ContentSchema,
@@ -33,6 +44,39 @@ export type Article = z.infer<typeof ArticleSchema>;
 
 /** An object containing the metadata and markdown of a today I Learned (TIL). */
 export type TodayILearned = z.infer<typeof TodayILearnedSchema>;
+
+/** An article that was written and has markdown content. */
+export type WrittenArticle = Extract<Article, { publisher: undefined }>;
+
+/** An article that references external content. */
+export type ExternalArticle = Extract<Article, { publisher: string }>;
+
+/** A note about an article. */
+export type ArticleNote = Extract<Note, { media: typeof ARTICLE_MEDIA }>;
+
+/** A note about an app. */
+export type AppNote = Extract<Note, { media: typeof APP_MEDIA }>;
+
+/** A note about a book. */
+export type BookNote = Extract<Note, { media: typeof BOOK_MEDIA }>;
+
+/** A note about a course. */
+export type CourseNote = Extract<Note, { media: typeof COURSE_MEDIA }>;
+
+/** A note about a live talk. */
+export type LiveTalkNote = Extract<Note, { media: typeof LIVE_TALK_MEDIA }>;
+
+/** A note about a podcast. */
+export type PodcastNote = Extract<Note, { media: typeof PODCAST_MEDIA }>;
+
+/** A note about a recorded talk. */
+export type RecordedTalkNote = Extract<Note, { media: typeof RECORDED_TALK_MEDIA }>;
+
+/** A note about a video. */
+export type VideoNote = Extract<Note, { media: typeof VIDEO_MEDIA }>;
+
+/** A note about a video playlist. */
+export type VideoPlaylistNote = Extract<Note, { media: typeof VIDEO_PLAYLIST_MEDIA }>;
 
 /** The coordinates of an object. */
 export type Coordinates = {
