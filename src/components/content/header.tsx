00a4a9789d602cec2bdc7301abed86e051cf8a94
Landon Schropp <schroppl@gmail.com>
2025-12-13 12:14:24 -0800

    Rework header styles

--- src/components/content/header.tsx
+++ src/components/content/header.tsx
@@ -3,18 +3,17 @@ import type { ReactNode } from "react";
 
 type HeaderLinkProps = {
   title: string;
-  className: string;
   href?: string | undefined;
 };
 
-function HeaderTitle({ title, className, href }: HeaderLinkProps) {
+function HeaderTitle({ title, href }: HeaderLinkProps) {
   if (href === undefined) {
-    return <span className={className} dangerouslySetInnerHTML={{ __html: title }} />;
+    return <span className="my-2 block" dangerouslySetInnerHTML={{ __html: title }} />;
   }
 
   return (
     <a
-      className={`${className} no-underline hover:underline`}
+      className={`my-2 block no-underline hover:underline`}
       href={href}
       dangerouslySetInnerHTML={{ __html: title }}
     />
@@ -22,7 +21,7 @@ function HeaderTitle({ title, className, href }: HeaderLinkProps) {
 }
 
 const SECONDARY_TEXT_CLASS_NAME =
-  "mx-auto text-base font-normal font-serif italic text-theme-lightText [&_a]:underline";
+  "mx-auto text-base font-normal font-serif italic text-theme-lightText [&_a]:underline my-2";
 
 type HeaderProps = {
   /** Short label or category text that appears above the title */
@@ -52,15 +51,15 @@ type HeaderProps = {
 export function Header({ kicker, title, titleHref, subtitle, tags, tagsIndexHref }: HeaderProps) {
   return (
     <header className="prose my-6 text-center">
-      <h1 className="my-0">
-        <span className={`${SECONDARY_TEXT_CLASS_NAME} my-1 block`}>{kicker}</span>{" "}
-        <HeaderTitle className="block" href={titleHref} title={title} />
+      <h1 className="my-2">
+        <span className={`${SECONDARY_TEXT_CLASS_NAME} block`}>{kicker}</span>{" "}
+        <HeaderTitle href={titleHref} title={title} />
       </h1>
       <div className={`${SECONDARY_TEXT_CLASS_NAME} my-2`} data-testid="subtitle">
         {subtitle}
       </div>
 
-      {tags && <Tags tags={tags} indexHref={tagsIndexHref} />}
+      {tags && <Tags className="my-2" tags={tags} indexHref={tagsIndexHref} />}
     </header>
   );
 }
